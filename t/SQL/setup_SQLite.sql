14763105f2b568adb192c13e7a6ba6da6d4234be
Guillaume Aubert <aubertg@cpan.org>
2012-11-10 19:06:34 -0500

    Added accounts table for testing JOINs.

--- t/SQL/setup_SQLite.sql
+++ t/SQL/setup_SQLite.sql
@@ -1,11 +1,23 @@
+-- Accounts table for use in JOIN tests --
+CREATE TABLE accounts
+(
+	account_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	email VARCHAR(128) NOT NULL,
+	created BIGINT(20) NOT NULL DEFAULT '0',
+	modified BIGINT(20) NOT NULL DEFAULT '0',
+	UNIQUE (email)
+);
+
 -- Standard table. --
 CREATE TABLE tests
 (
 	test_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name VARCHAR(32) NOT NULL,
 	value VARCHAR(128) DEFAULT NULL,
+	account_id INTEGER DEFAULT NULL,
 	created BIGINT(20) NOT NULL DEFAULT '0',
 	modified BIGINT(20) NOT NULL DEFAULT '0',
+	FOREIGN KEY (account_id) REFERENCES accounts(account_id),
 	UNIQUE (name)
 );
 
@@ -28,3 +40,4 @@ CREATE TABLE no_modified_tests
 	created BIGINT(20) NOT NULL DEFAULT '0',
 	UNIQUE (name)
 );
+
--- t/SQL/setup_mysql.sql
+++ t/SQL/setup_mysql.sql
@@ -1,9 +1,20 @@
+-- Accounts table for use in JOIN tests --
+CREATE TABLE accounts
+(
+	account_id bigint(20) unsigned NOT NULL auto_increment,
+	email varchar(128) NOT NULL,
+	created bigint(20) NOT NULL default '0',
+	modified bigint(20) NOT NULL default '0',
+	UNIQUE KEY idx_unique_email (email)
+);
+
 -- Standard table. --
 CREATE TABLE tests
 (
 	test_id bigint(20) unsigned NOT NULL auto_increment,
 	name varchar(32) NOT NULL,
 	value varchar(128) DEFAULT NULL,
+	account_id bigint(20) unsigned DEFAUL NULL REFERENCES accounts(account_id),
 	created bigint(20) unsigned NOT NULL default '0',
 	modified bigint(20) unsigned NOT NULL default '0',
 	PRIMARY KEY (test_id),
