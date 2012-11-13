eeac515fc1fbee6468fcc819cb7e34c60d85b47b
Guillaume Aubert <aubertg@cpan.org>
2012-11-13 01:27:54 -0500

    Added POD skeletton.

---
+++ lib/DBIx/NinjaORM.pm
@@ -0,0 +1,124 @@
+package DBIx::NinjaORM;
+
+use warnings;
+use strict;
+
+use Carp;
+use Data::Dumper;
+
+
+=head1 NAME
+
+DBIx::NinjaORM - Flexible Perl ORM for easy transitions from inline SQL to objects.
+
+
+=head1 VERSION
+
+Version 1.0.0
+
+=cut
+
+our $VERSION = '1.0.0';
+
+
+=head1 SYNOPSIS
+
+#TODO Write synopsis.
+
+
+=head1 SUPPORTED DATABASES
+
+This distribution currently supports:
+
+=over 4
+
+=item * SQLite
+
+=back
+
+Please contact me if you need support for another database type, I'm always
+glad to add extensions if you can help me with testing.
+
+
+=head1 SUBCLASSABLE METHODS
+
+
+=head1 UTILITY METHODS
+
+
+=head1 ACCESSORS
+
+
+=head1 CACHE RELATED METHODS
+
+
+=head1 INTERNAL METHODS
+
+
+=head1 AUTHOR
+
+Guillaume Aubert, C<< <aubertg at cpan.org> >>.
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-dbix-ninjaorm at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBIx-NinjaORM>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+	perldoc DBIx::NinjaORM
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=DBIx-NinjaORM>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/DBIx-NinjaORM>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/DBIx-NinjaORM>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/DBIx-NinjaORM/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+Thanks to ThinkGeek (L<http://www.thinkgeek.com/>) and its corporate overlords
+at Geeknet (L<http://www.geek.net/>), for footing the bill while I write code
+for them!
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009-2012 Guillaume Aubert.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License version 3 as published by the Free
+Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see http://www.gnu.org/licenses/
+
+=cut
+
+1;
