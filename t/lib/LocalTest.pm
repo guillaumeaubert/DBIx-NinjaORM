aa385f57b16b07580df8d9def9743884a804709b
Guillaume Aubert <aubertg@cpan.org>
2012-11-26 10:54:08 -0500

    Released version 2.1.1.

--- t/lib/LocalTest.pm
+++ t/lib/LocalTest.pm
@@ -17,11 +17,11 @@ LocalTest - Test functions for L<DBIx::NinjaORM>.
 
 =head1 VERSION
 
-Version 2.1.0
+Version 2.1.1
 
 =cut
 
-our $VERSION = '2.1.0';
+our $VERSION = '2.1.1';
 
 
 =head1 SYNOPSIS
