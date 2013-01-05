776c9c7e9cbc6102e190fdb768286388605609f2
Guillaume Aubert <aubertg@cpan.org>
2013-01-05 10:03:25 -0500

    Updated copyright years.

--- lib/DBIx/NinjaORM.pm
+++ lib/DBIx/NinjaORM.pm
@@ -3073,7 +3073,7 @@ Thanks to Jamie McCarthy for adding the 'ignore' argument to C<insert()>.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009-2012 Guillaume Aubert.
+Copyright 2009-2013 Guillaume Aubert.
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License version 3 as published by the Free
