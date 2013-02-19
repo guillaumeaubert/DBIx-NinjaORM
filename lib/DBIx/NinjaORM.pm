161ae868c594bf0fbe30655ba5159aeedb75a8df
Guillaume Aubert <aubertg@cpan.org>
2013-02-19 14:23:30 -0500

    Released version 2.4.0.

--- lib/DBIx/NinjaORM.pm
+++ lib/DBIx/NinjaORM.pm
@@ -22,11 +22,11 @@ DBIx::NinjaORM - Flexible Perl ORM for easy transitions from inline SQL to objec
 
 =head1 VERSION
 
-Version 2.3.2
+Version 2.4.0
 
 =cut
 
-our $VERSION = '2.3.2';
+our $VERSION = '2.4.0';
 
 
 =head1 DESCRIPTION
--- t/lib/LocalTest.pm
+++ t/lib/LocalTest.pm
@@ -17,11 +17,11 @@ LocalTest - Test functions for L<DBIx::NinjaORM>.
 
 =head1 VERSION
 
-Version 2.3.2
+Version 2.4.0
 
 =cut
 
-our $VERSION = '2.3.2';
+our $VERSION = '2.4.0';
 
 
 =head1 SYNOPSIS
