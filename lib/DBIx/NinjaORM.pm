2caed983354ddccab9d5b67bb4632bc43294d1b3
Guillaume Aubert <aubertg@cpan.org>
2013-03-17 18:39:40 -0400

    Changed RT links to point to GitHub.

--- lib/DBIx/NinjaORM.pm
+++ lib/DBIx/NinjaORM.pm
@@ -3056,9 +3056,10 @@ Guillaume Aubert, C<< <aubertg at cpan.org> >>.
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-dbix-ninjaorm at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBIx-NinjaORM>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Please report any bugs or feature requests through the web interface at
+L<https://github.com/guillaumeaubert/DBIx-NinjaORM/issues/new>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
 
 
 =head1 SUPPORT
@@ -3072,9 +3073,9 @@ You can also look for information at:
 
 =over 4
 
-=item * RT: CPAN's request tracker
+=item * GitHub's request tracker
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=DBIx-NinjaORM>
+L<https://github.com/guillaumeaubert/DBIx-NinjaORM/issues>
 
 =item * AnnoCPAN: Annotated CPAN documentation
 
